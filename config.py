b074e17fc37bde0f2a1eeee0bd28ca957f790cb8
Jexxy113 <viveksham62033@gmail.com>
2024-09-04 17:02:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,12 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
-
+BOT_TOKEN=("7201156025:AAEV_2SBwk8DmUfk7EEF5iaajx1zA-PjO8A")
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Vibhusham62033")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5431393990")
 
 
 
